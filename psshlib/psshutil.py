8fbad23b606af8cd3ec66628a05ac37920e49dbb
Andrew McNabb <amcnabb@mcnabbs.org>
2011-01-10 07:33:26 -0700

    fixed comment line processing in host files (issue #27)

--- psshlib/psshutil.py
+++ psshlib/psshutil.py
@@ -26,8 +26,8 @@ def read_hosts(pathnames, default_user=None, default_port=None):
     hosts = []
     for line in lines:
         # Skip blank lines or lines starting with #
-        line = line.strip(string.whitespace + '#')
-        if not line:
+        line = line.strip()
+        if not line or line.startswith('#'):
             continue
         host, port, user = parse_line(line, default_user, default_port)
         if host:
